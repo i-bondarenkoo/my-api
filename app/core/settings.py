6da0d35c5570a21bf77797726e554469dec72d48
Igor Bondarenko <bondarenko200021@mail.ru>
2025-04-13 20:25:33 +0700

    add pytests

--- app/core/settings.py
+++ app/core/settings.py
@@ -3,7 +3,10 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
 
+    # основная база данных
     db_url: str = "postgresql+asyncpg://postgres:postgres@localhost:5439/postgres"
+    # тестовая база данных
+    test_db_url: str = "postgresql+asyncpg://postgres:postgres@localhost:5440/test_db"
     db_echo: bool = True
 
 
---
+++ tests/test_database.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from app.core.settings import settings
+
+test_engine = create_async_engine(
+    url=settings.test_db_url,
+    echo=settings.db_echo,
+)
+
+# фабрика сессий
+TestSession = async_sessionmaker(bind=test_engine, expire_on_commit=False)
+
+
+async def get_testdb_sesion():
+    async with TestSession() as test_session:
+        yield test_session
