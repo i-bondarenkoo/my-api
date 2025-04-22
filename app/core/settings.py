34244580e4a577eb87ec03525390375b4f83ddac
Igor Bondarenko <bondarenko200021@mail.ru>
2025-04-23 01:18:59 +0700

    fix errors

--- app/core/settings.py
+++ app/core/settings.py
@@ -9,5 +9,9 @@ class Settings(BaseSettings):
     test_db_url: str = "postgresql+asyncpg://postgres:postgres@localhost:5440/test_db"
     db_echo: bool = True
 
+    secret_key: str = "e7e2385e08e4d3d2905ddca286ecdabfb6a00169a44ff328f4a5e878b8e1e9c7"
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
+
 
 settings = Settings()
