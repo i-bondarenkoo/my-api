c375b52496c1874f85d1797d9d44bd70b91c6468
Igor Bondarenko <bondarenko200021@mail.ru>
2025-04-30 16:58:44 +0700

    upd templates

--- app/templates/users/views.py
+++ app/templates/users/views.py
@@ -6,7 +6,7 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.database import get_db_session
 from app import crud
-
+from app.schemas.user import CreateUser
 
 router = APIRouter(prefix="/pages", tags=["Pages"])
 
@@ -65,4 +65,41 @@ async def tasks_list(
             "stop": stop,
             "step": step,
         },
+    )
+
+
+@router.get("/projects", response_class=HTMLResponse)
+async def projects_list(
+    request: Request,
+    start: int = 0,
+    stop: int = 5,
+    session: AsyncSession = Depends(get_db_session),
+):
+    projects_list = await crud.get_list_projects_crud(
+        start=start, stop=stop, session=session
+    )
+    step = stop - start
+    return templates.TemplateResponse(
+        request=request,
+        name="projects_list.html",
+        context={
+            "projects": projects_list,
+            "start": start,
+            "stop": stop,
+            "step": step,
+        },
+    )
+
+
+@router.post("/users/create-user", response_class=HTMLResponse)
+async def make_new_user(
+    request: Request,
+    user_data: CreateUser,
+    session: AsyncSession = Depends(get_db_session),
+):
+    new_user = await crud.create_user_crud(user=user_data, session=session)
+    return templates.TemplateResponse(
+        request=request,
+        name="create_user.html",
+        context={},
     )
